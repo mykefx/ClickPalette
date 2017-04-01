4257d196b29fb1302d8b0051aa7165d2c751ba46
James Newman <j@mesnewman.co>
2017-04-01 12:46:39 +0100

    1.1 Ready to build

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function createWindow(){
     mainWindow.setAlwaysOnTop(true);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     mainWindow.on('close', function(e) {
         var bounds = mainWindow.getBounds();
